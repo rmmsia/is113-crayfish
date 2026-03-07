7e61b1005eb0b58f2ba403f196f6a606870a073d
HawYanTzer-SMU <hawyantzer03@gmail.com>
2026-03-07 22:26:38 +0800

    feat: invitation static page

--- app.js
+++ app.js
@@ -61,6 +61,32 @@ server.get('/profile', (req, res) => {
   })
 })
 
+//Invitation Page 
+server.get('/invitation', (req, res) => {
+  res.render("invitation", {
+    invite : null
+  })
+})
+
+//Generate-invitation Process
+server.post('/generate-invitation', (req,res) => {
+  const inviteCode = Math.random().toString(36).substring(2, 10);
+  const inviteLink = `http://localhost:3000/register?invite=${inviteCode}`;
+
+  const invite = {
+    inviteID: inviteCode, 
+    sent: true,
+    status: "pending", 
+    invitedUser: "Li Jiannan", 
+    inviteLink
+  }
+
+  res.render("invitation", {
+    invite
+  });
+
+})
+
 // Route to home page sorted by top posts by default
 server.get('/home', (req, res) => {
   // TBD: some function to sort posts before rendering them?
