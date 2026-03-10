37a895328e38caba6849cad1b0b595e96b03c6bd
Ryan <rmmsia@gmail.com>
2026-03-10 17:04:01 +0800

    feat: implemented invite code generation

--- routes/invite.js
+++ routes/invite.js
@@ -1,30 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const { requireLogin } = require('../middleware/auth')
+const Invite = require('../models/Invite');
 
 //Invitation Page 
-router.get('/invitation', (req, res) => {
+router.get('/invitation', requireLogin, (req, res) => {
   res.render("invitation", {
-    invite : null
+    invite : null,
+    email: null,
+    error: null
   })
 })
 
 //Generate-invitation Process
-router.post('/generate-invitation', (req,res) => {
+router.post('/generate-invitation', requireLogin, async (req, res) => {
+  const email = req.body.email;
   const inviteCode = Math.random().toString(36).substring(2, 10);
-  const inviteLink = `http://localhost:3000/register?invite=${inviteCode}`;
 
-  const invite = {
-    inviteID: inviteCode, 
-    sent: true,
-    status: "pending", 
-    invitedUser: "Li Jiannan", 
-    inviteLink
+  try {
+    // Check if invite already exists
+    const existingInvite = await Invite.findOne({ targetEmail: email });
+
+    if (existingInvite) {
+      return res.render("invitation", {
+        error: "This email already has an invite.",
+        email: email,
+        invite: null
+      });
+    }
+
+    const newInvite = new Invite({
+      createdBy: req.user.username,
+      targetEmail: email,
+      status: "Pending",
+      code: inviteCode
+    });
+
+    await newInvite.save();
+
+    res.render("invitation", {
+      invite: {
+        inviteID: inviteCode,
+        status: "Pending",
+        invitedUser: email
+      },
+      error: null,
+      email
+    });
+
+  } catch (err) {
+    res.status(500).send("Error sending invite: " + err.message);
   }
-
-  res.render("invitation", {
-    invite
-  });
-
-})
+});
 
 module.exports = router;
