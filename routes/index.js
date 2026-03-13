c7e945bb86219ac4c0e9c82834091cef95856f26
ivusc <ivuschua@gmail.com>
2026-03-13 23:36:51 +0800

    feat: dynamic profile page

--- routes/index.js
+++ routes/index.js
@@ -1,65 +1,96 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 const { requireLogin } = require('../middleware/auth');
+const User = require('../models/User');
 
-router.get('/profile', (req , res) => {
-  const user = {
-    username: "username",
-    invite: "admin",
-    karma: 67,
-    totalPosts: 2,
-    totalComments: 120,
-    about: "I was dropped as a child",
-    joinedAt: "2026-03-05T07:00:00Z"
+async function retrieveUserData(username) {
+  const posts = await Post.find({ author: username });
+  const totalPosts = posts.length || 0;
+  const totalComments = await Comment.find({ author: username }).countDocuments() || 0;
+  const totalKarma = posts.reduce((karma, post) => karma + ((post.upvotes - post.downvotes) || 0), 0);
+
+  return {
+    totalPosts,
+    totalComments,
+    totalKarma
   }
+}
+
+//WHEN VISITING YOUR PROFILE
+router.get('/profile', requireLogin, async (req , res) => {
+  const user = req.user;
+  const { totalPosts, totalComments, totalKarma } = await retrieveUserData(user.username);
   
-  res.render("profile", {
+  res.render("profile/profile", {
+    user,
+    totalKarma,
+    totalPosts,
+    totalComments,
+    isUser: true
+  })
+})
+
+//WHEN VISITING OTHER PROFILES
+router.get('/profile/:username', requireLogin, async (req, res) => {
+  const { username } = req.params;
+
+  if (username === req.user.username) return res.redirect('/profile');
+  
+  try{
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(404).send('User not found.');
+    
+    const { totalPosts, totalComments, totalKarma } = await retrieveUserData(user.username);
+
+    res.render("profile/profile", {
+      user,
+      totalKarma,
+      totalPosts,
+      totalComments,
+      isUser: false
+    })
+  } catch (err) {
+    console.error('Error retrieving profile: ' + err);
+    res.status(500).send(err.message);
+  }
+})
+
+//UPDATE PROFILE
+router.get('/update-profile', requireLogin, async (req, res) => {
+  const user = req.user;
+  
+  res.render('profile/update-profile', {
     user
   })
 })
 
-// remove and replace with DB retrieval once implemented
-let posts = [
-  {
-    id: 1,
-    title: "haha so funny",
-    imageURL: "https://media.tenor.com/BuSEbkm9aAIAAAAi/hi-otag.gif",
-    //redirectURL: "",  -- needed?
-    author: "admin",
-    upvotes: 4,
-    downvotes: 0,
-    // tags: ["funny, idk"],  -- if got time to implement tags
-    comments: [
-      {author: "admin2", body: "haha", datetime: "2026-03-04T09:00:00Z"}, // datetime using ISO 8601 format, with Z at the end indicating UTC timezone
-      {author: "admin3", body: "not funny bro", datetime: "2026-03-04T12:00:00Z"} // datetime using ISO 8601 format, with Z at the end indicating UTC timezone
-    ],
-    datetime: "2026-03-04T05:00:00Z" // datetime using ISO 8601 format, with Z at the end indicating UTC timezone
-  },
-  {
-    id: 2,
-    title: "funny",
-    imageURL: "https://media1.tenor.com/m/B-uBLzMr9BwAAAAd/low-cortisol.gif",
-    //redirectURL: "",  -- needed?
-    author: "admin3",
-    upvotes: 2,
-    downvotes: 3,
-    // tags: ["funny, idk"],  -- if got time to implement tags
-    comments: [
-      {author: "admin2", body: "test", datetime: "2026-03-06T02:00:00Z"} // datetime using ISO 8601 format, with Z at the end indicating UTC timezone
-    ],
-    datetime: "2026-03-05T07:00:00Z" // datetime using ISO 8601 format, with Z at the end indicating UTC timezone
+router.post('/update-profile', requireLogin, async (req, res) => {
+  const { about } = req.body;
+  const user = req.user;
+
+  try {
+    const newUser = await User.findByIdAndUpdate(user._id, { about: about });
+
+    if (!newUser) return res.status(404).send('User not found.');
+
+    res.redirect('/profile');
+  } catch (err) {
+    console.error('Error updating profile: ' + err);
+    res.status(500).send(err.message)
   }
-];
+})
 
 router.get('/home', requireLogin, async (req, res) => {
-    try {
-        const posts = await Post.find().sort({ createdAt: -1 });
-        res.render('home', { posts });
-    } catch (err) {
-        console.error('Error rendering /home:', err); // <- this will show the real reason for 500
-        res.status(500).send(err.message);
-    }
+  try {
+      const posts = await Post.find().sort({ createdAt: -1 });
+      res.render('home', { posts });
+  } catch (err) {
+      console.error('Error rendering /home:', err); // <- this will show the real reason for 500
+      res.status(500).send(err.message);
+  }
 });
 
 module.exports = router;
