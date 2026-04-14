ac221bd197676dc5c19e22ec382bd1b9ee006563
Ryan <rmmsia@gmail.com>
2026-04-14 15:51:40 +0800

    feat: user tree navigation

--- services/userTree.js
+++ services/userTree.js
@@ -1,11 +1,23 @@
 const User = require('../models/User');
+const Post = require('../models/Post');
 
 async function getUserTree() {
   const users = await User.find({}).lean();
 
+  // Fetch all posts to calculate karma
+  const posts = await Post.find({}).lean();
+
+  // Calculate karma for each user
+  const karmaMap = {};
+  posts.forEach(post => {
+    const karma = (post.upvotes?.length || 0) - (post.downvotes?.length || 0);
+    karmaMap[post.author] = (karmaMap[post.author] || 0) + karma;
+  });
+
   const userMap = {};
   users.forEach(u => {
     u.children = [];
+    u.karma = karmaMap[u.username] || 0;
     userMap[u.username] = u;
   });
 
